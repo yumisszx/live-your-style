f4e23188e13f4c6b3361e43e8d52ec36e905bcdb
unknown <ligiayumimeira@gmail.com>
2025-12-09 21:27:05 -0300

    atualizando botao de sumir

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,8 @@
-const some1 = document.querySelector(".someCores");
-const some2 = document.querySelector(".someEstilos");
-const some3 = document.querySelector(".someCustomizacao");
+//função sumir botões
+const some1 = document.querySelector("#some");
 
 const ligarEDesligarADiv = function(){
 
     some1.style.display === "flex" ? some1.style.display = "none": some1.style.display = "flex";
-    some2.style.display === "flex" ? some2.style.display = "none": some2.style.display = "flex";
-    some3.style.display === "flex" ? some3.style.display = "none": some3.style.display = "flex";
 
 };
