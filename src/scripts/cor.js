f214ff5b33a5cd121f3ed94718ea1709dbc13595
unknown <ligiayumimeira@gmail.com>
2025-12-11 23:32:09 -0300

    adicionando js

--- src/scripts/cor.js
+++ src/scripts/cor.js
@@ -1,1 +1,8 @@
 //função sumir texto das cores
+const some = document.querySelector(".nav-txt");
+
+const ligarCor = function(){
+
+    some.style.display === "flex" ? some.style.display = "none": some.style.display = "flex";
+
+};
