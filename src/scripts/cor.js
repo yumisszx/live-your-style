f2f20829b7654edf37d34f02e3b266c5c68eb8d7
unknown <ligiayumimeira@gmail.com>
2025-12-17 17:52:44 -0300

    função js guias de conteudo página cor

--- src/scripts/cor.js
+++ src/scripts/cor.js
@@ -1,8 +1,18 @@
-//função sumir texto das cores
-//const some = document.querySelector(".nav-txt");
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCor = document.querySelectorAll('.btn-cor');
+    const txtCor = document.querySelectorAll('.nav-txt');
 
-const ligarCor = function(){
+    btnCor.forEach(button => {
+        button.addEventListener('click', () => {
+            //remover ativo
+            btnCor.forEach(btnCor  => btnCor.classList.remove('ativo'));
+            txtCor.forEach(txtCor => txtCor.classList.remove('ativo'));
 
-    some.style.display === "flex" ? some.style.display = "none": some.style.display = "flex";
+            //colocar ativo no clicado
+            button.classList.add('ativo');
+            const cor = button.getAttribute('data-cor');
+            document.getElementById(cor).classList.add('ativo');
 
-};
+        });
+    });
+});
